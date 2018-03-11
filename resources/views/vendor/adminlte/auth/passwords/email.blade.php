23fbcee16e61cfada106014ba80c09e2a6f69aa7
Heles Junior <helesjunior@gmail.com>
2018-03-11 17:43:52 -0300

    updatehome

--- resources/views/vendor/adminlte/auth/passwords/email.blade.php
+++ resources/views/vendor/adminlte/auth/passwords/email.blade.php
@@ -22,7 +22,7 @@
 
         @if (count($errors) > 0)
             <div class="alert alert-danger">
-                <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
+                <strong>Ops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
                 <ul>
                     @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
